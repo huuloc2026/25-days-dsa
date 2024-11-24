04d2aefb3ddca4eb3ba7eda91404dab0217f54e4
huuloc2026 <huuloc2026@gmail.com>
2024-11-24 19:56:52 +0700

    move zero

---
+++ day5/dynamicarray/MoveZero.py
@@ -0,0 +1,16 @@
+def moveZeroes(nums):
+    #[0,1,0,3,12]
+    #   L
+    #     R
+    left = 0
+    for right in range(len(nums)):
+        if nums[right]!=0:
+            nums[left],nums[right] = nums[right],nums[left]
+            left+=1
+        right+=1
+
+
+
+nums = [0,1,0,3,12]
+moveZeroes(nums)
+print(nums)
